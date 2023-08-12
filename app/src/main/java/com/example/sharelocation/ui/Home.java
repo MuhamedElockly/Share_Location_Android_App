84f4359db3561afdc20e47a9cfcf792e2b14a925
dell <elocklymuhamed@gmail.com>
2023-08-12 21:40:03 +0200

    google login

--- app/src/main/java/com/example/sharelocation/ui/Home.java
+++ app/src/main/java/com/example/sharelocation/ui/Home.java
@@ -492,6 +492,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         finish();
         startActivity(intent);
+
     }
 
     private void addRoom() {
