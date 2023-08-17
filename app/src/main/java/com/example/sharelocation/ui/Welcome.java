bf3391cca6d961887a622f5141c5b0c7bfaf3647
dell <elocklymuhamed@gmail.com>
2023-08-17 22:50:10 +0200

    add dynamic linking

--- app/src/main/java/com/example/sharelocation/ui/Welcome.java
+++ app/src/main/java/com/example/sharelocation/ui/Welcome.java
@@ -1,15 +1,20 @@
 package com.example.sharelocation.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -22,16 +27,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
 import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 
 public class Welcome extends AppCompatActivity {
     ActivityWelcomeBinding binding;
+    DatabaseReference database;
     private FirebaseAuth fAuth;
     private FirebaseUser user;
     private DatabaseReference roomRef;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +72,7 @@ public class Welcome extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
-        // recieveLink();
+        database = FirebaseDatabase.getInstance().getReference("roomMembers");
     }
 
     @Override
@@ -85,12 +94,14 @@ public class Welcome extends AppCompatActivity {
     }
 
     private void recieveLink() {
+
         FirebaseDynamicLinks.getInstance().getDynamicLink(getIntent()).addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
             @Override
             public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
                 // Get deep link from result (may be null if no link is found)
                 Uri deepLink = null;
                 if (pendingDynamicLinkData != null) {
+                    showProgreesBar();
                     deepLink = pendingDynamicLinkData.getLink();
 
                     Log.d("recivedLink", deepLink.toString());
@@ -101,7 +112,28 @@ public class Welcome extends AppCompatActivity {
 
                         String roomId = referedLink.substring(0, referedLink.indexOf("@"));
                         String roomName = referedLink.substring(referedLink.lastIndexOf("@") + 1);
-                        pushUserToFireBase(roomId, roomName);
+
+                        database.child(roomId).orderByChild("id").equalTo(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists()) {
+                                    dialog.cancel();
+                                    openNewRoom(roomId, roomName);
+                                } else {
+                                    pushUserToFireBase(roomId, roomName);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                dialog.cancel();
+                                Intent intent = new Intent(getApplicationContext(), Home.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                finish();
+                                startActivity(intent);
+                            }
+                        });
+
 
                         Log.d("roomId", referedLink);
 
@@ -158,6 +190,7 @@ public class Welcome extends AppCompatActivity {
         roomRef.child(roomId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
+                dialog.cancel();
                 if (task.isSuccessful()) {
                     DataSnapshot snapshot = task.getResult();
                     String lastRoomCapcity = snapshot.child("roomCapacity").getValue(String.class);
@@ -167,6 +200,19 @@ public class Welcome extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void showProgreesBar() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.load_dialoge, null);
+        ProgressBar progressBar = view.findViewById(R.id.profilePbar);
+
+        builder.setView(view);
+        dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+
     }
 
 
