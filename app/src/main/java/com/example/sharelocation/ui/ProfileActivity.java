a63c5b138f6cf0fa3978677e549c10564b97a2e1
dell <elocklymuhamed@gmail.com>
2023-08-24 18:55:25 +0200

    handle profile user exception

--- app/src/main/java/com/example/sharelocation/ui/ProfileActivity.java
+++ app/src/main/java/com/example/sharelocation/ui/ProfileActivity.java
@@ -1,15 +1,20 @@
 package com.example.sharelocation.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -38,8 +43,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-
-import net.yslibrary.android.keyboardvisibilityevent.util.UIUtil;
 
 public class ProfileActivity extends AppCompatActivity {
     AlertDialog dialog;
@@ -72,7 +75,6 @@ public class ProfileActivity extends AppCompatActivity {
         userId = user.getUid();
 
 
-        refresh();
         binding.swipeToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -87,7 +89,11 @@ public class ProfileActivity extends AppCompatActivity {
         imagePicker();
         updatPassward();
         forgetPassward();
-
+        if (!isNetworkAvailable()) {
+            showConfirmationDialoge("Please check internet connection !");
+            return;
+        }
+        refresh();
     }
 
     private void updatePasswardView() {
@@ -225,6 +231,10 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     private void refresh() {
+        if (!isNetworkAvailable()) {
+            showConfirmationDialoge("Please check internet connection !");
+            return;
+        }
 
         userRef.child(userId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
@@ -405,5 +415,28 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
 
+    private void showConfirmationDialoge(String erorrMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.erorr_dialoge, null);
+        TextView alertMessage = view.findViewById(R.id.errorBody);
+        alertMessage.setText(erorrMessage);
+        Button dialogeOk = view.findViewById(R.id.ok);
+        builder.setView(view);
+        final AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+        dialogeOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 
 }
