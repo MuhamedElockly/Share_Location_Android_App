fa296541db90001741044a6f1e1d3aae9253bd7e
dell <elocklymuhamed@gmail.com>
2023-08-30 19:41:55 +0200

    update sign in model

--- app/src/main/java/com/example/sharelocation/ui/SignUp.java
+++ app/src/main/java/com/example/sharelocation/ui/SignUp.java
@@ -26,6 +26,7 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.sharelocation.R;
 import com.example.sharelocation.databinding.ActivitySignUpBinding;
 import com.example.sharelocation.pojo.UserModel;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -100,6 +101,18 @@ public class SignUp extends AppCompatActivity {
         startActivity(intent);
 
 
+    }
+    private void imagePicker() {
+        binding.editProfilePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(ProfileActivity.this).crop()                    //Crop image(Optional), Check Customization for more option
+                        .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+
+            }
+        });
     }
 
     @Override
